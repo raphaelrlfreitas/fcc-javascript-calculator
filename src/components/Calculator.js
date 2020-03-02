01e6f5388048d3a0b35a1db75f3187ee8f44987d
Raphael Freitas <rrabbi@hotmail.com>
2020-03-02 22:20:53 +0100

    Challange complete

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,12 +3,11 @@ import './../styles/Calculator.css';
 
 const initialState = {
   currentVal: '0',
-  previousVal: '0',
   formula: '',
-  currentSign: 'pos',
+  evaluated: false,
 };
 
- const endsWithOperator = /[-+x/]$/,
+const endsWithOperator = /[-+x/]$/,
        endsWithNegative = /[/+x]-$/,
        isOperator = /[-+/x]/;
 
@@ -23,25 +22,56 @@ class Calculator extends React.Component {
     this.handleNumbers = this.handleNumbers.bind(this);
     this.handleDecimal = this.handleDecimal.bind(this);
     this.handleOperator = this.handleOperator.bind(this);
+    this.handleEvaluate = this.handleEvaluate.bind(this);
   }
 
   handleClear() {
     this.setState(initialState);
   }
 
+  handleOperator(e) {
+    const { currentVal, formula, evaluated} = this.state;
+
+    this.setState({
+      formula:
+        (formula === '' || evaluated) ?
+          currentVal.concat(e.target.innerHTML) 
+        : (endsWithOperator.test(formula) && isOperator.test(currentVal) && e.target.innerHTML === '-') ?
+          formula.concat(e.target.innerHTML) 
+        : (endsWithNegative.test(formula) && isOperator.test(currentVal)) ?
+          formula.slice(0, -2).concat(e.target.innerHTML)  
+        : (endsWithOperator.test(formula) && isOperator.test(currentVal)) ?
+          formula.slice(0, -1).concat(e.target.innerHTML)  
+        :
+          formula.concat(currentVal,e.target.innerHTML),
+
+      currentVal: e.target.innerHTML,
+      evaluated: false,
+    })
+  }
+  
   handleNumbers(e) {
 
-    const {currentVal} = this.state;
+    const {currentVal, evaluated} = this.state;
 
+    evaluated === true ?
+      this.setState({
+        formula: '',
+        currentVal: e.target.innerHTML,
+        evaluated: false,
+      })
+    :
     currentVal === '0' ?
       this.setState({
         currentVal: e.target.innerHTML,
-        previousVal: currentVal
+      })
+    : isOperator.test(currentVal) ?
+      this.setState({
+        currentVal: e.target.innerHTML
       })
     :
       this.setState({
         currentVal: currentVal.concat(e.target.innerHTML),
-        previousVal: currentVal
       });
   }
 
@@ -53,21 +83,23 @@ class Calculator extends React.Component {
         currentVal: currentVal.concat('.'),
       })
     }
-
   }
 
-  handleOperator(e) {
-    const { currentVal, formula} = this.state;
+  handleEvaluate(e) {
+
+    const {formula, currentVal} = this.state;
+
+    const result = eval(formula.replace('x', '*').concat(currentVal));
+
 
     this.setState({
-      formula:
-        formula === '' ?
-        currentVal.concat(e.target.innerHTML) :
-        formula.concat(currentVal),
-
-      currentVal: '0',
+      formula: formula.concat(currentVal, e.target.innerHTML, result),
+      currentVal: result.toString(),
+      evaluated: true,
     })
+
   }
+
 
   render() {
     return (
@@ -91,7 +123,7 @@ class Calculator extends React.Component {
         <div id="two" value="2" onClick={this.handleNumbers}  className="dark-grey-bg two center-content">2</div>
         <div id="one" value="1" onClick={this.handleNumbers}  className="dark-grey-bg one center-content">1</div>
         <div id="zero" value="0" onClick={this.handleNumbers}  className="dark-grey-bg zero center-content">0</div>
-        <div id="equals" value="=" className="dark-blue-bg equal center-content">=</div>
+        <div id="equals" value="=" onClick={this.handleEvaluate} className="dark-blue-bg equal center-content">=</div>
         <div id="decimal" value="." onClick={this.handleDecimal} className="dark-grey-bg decimal center-content">.</div>
         
       </div>
